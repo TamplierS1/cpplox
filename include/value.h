4e25cd91a4ac20e88a665125e8eeea9dee9b558f
Azamat Nabiullin <azamat.nabiullin.97@mail.ru>
2021-03-21 11:43:14 +0300

    Added comments
    Added comments
    `parenthesize` method in `AstPrinter` now doesn't return when it encounters an unhandled variant

--- include/value.h
+++ include/value.h
@@ -8,8 +8,11 @@
 
 namespace garm
 {
+
+// Value represents a literal value that some expressions can have - currently either a string or a double
 using Value = std::optional<std::variant<std::string, double>>;
 
+// Converts Value to string (it's used when printing tokens)
 std::optional<std::string> literal_as_string(const Value& literal);
 
 }
